5ea92d2717a915fea4bcec91515ab231e76c96f2
Krish Moorjani <krishmoorjani04@gmail.com>
2025-09-05 13:51:20 +0530

    chore: logging setup

--- email_campaign/settings.py
+++ email_campaign/settings.py
@@ -24,7 +24,7 @@ SMTP_HOST = os.getenv("SMTP_HOST", "smtp.mailgun.org")
 SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_USER = os.getenv("SMTP_USER", "")
 SMTP_PASS = os.getenv("SMTP_PASS", "")
-FROM_EMAIL = os.getenv("FROM_EMAIL", "noreply@mikelegal.com")
+FROM_EMAIL = os.getenv("FROM_EMAIL", "mailtest@abc.com")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -132,3 +132,38 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "root": {  
+        "handlers": ["console"],
+        "level": "INFO", 
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",   # Django internals
+            "propagate": False,
+        },
+        "campaigns": {
+            "handlers": ["console"],
+            "level": "DEBUG", 
+            "propagate": False,
+        },
+    },
+}
